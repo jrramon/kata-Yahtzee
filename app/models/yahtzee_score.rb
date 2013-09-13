28b1c14bad0af7e3e0ad55ca35ca56fc4e4e1e41
Joserra <jrramon@gmail.com>
2013-09-13 11:15:09 +0200

    Easy solution for :ones

--- app/models/yahtzee_score.rb
+++ app/models/yahtzee_score.rb
@@ -3,9 +3,13 @@ class Yahtzee_score
 	def points(category, dices)
 		case category
 			when :ones
-				return 1
+				return sum_of_ones(dices)
 			when :twos
 				return 6
 		end
+	end
+
+	def sum_of_ones(dices)
+		dices.count{|die| die == 1 }
 	end
 end
