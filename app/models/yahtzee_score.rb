7cb213066dfecf485c76b8a453c22b09370794da
Joserra <jrramon@gmail.com>
2013-09-13 11:18:45 +0200

    green for twos

--- app/models/yahtzee_score.rb
+++ app/models/yahtzee_score.rb
@@ -5,11 +5,15 @@ class Yahtzee_score
 			when :ones
 				return sum_of_ones(dices)
 			when :twos
-				return 6
+				return sum_of_twos(dices)
 		end
 	end
 
 	def sum_of_ones(dices)
 		dices.count{|die| die == 1 }
+	end
+
+	def sum_of_twos(dices)
+		dices.count{|die| die == 2 } * 2
 	end
 end
