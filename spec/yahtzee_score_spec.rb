f34b39f580f62c448e3ea78e1f4f17ffe5f01135
Joserra <jrramon@gmail.com>
2013-09-13 11:11:45 +0200

    Testing by troubled branch

--- spec/yahtzee_score_spec.rb
+++ spec/yahtzee_score_spec.rb
@@ -6,6 +6,7 @@ describe Yahtzee_score, "#score" do
 	describe "for categories of numbers" do
 		it "the sum for ONES" do
 			yahtzee_score.points(:ones, [1,2,2,2,2]).should eq (1)
+			yahtzee_score.points(:ones, [1,2,1,2,2]).should eq (2)
 		end
 		it "the sum for TWOS" do
 			yahtzee_score.points(:twos, [1,2,1,2,2]).should eq (6)
