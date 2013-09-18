d772de9a4899734af5333f509da46e636f238e6b
Joserra <jrramon@gmail.com>
2013-09-18 15:15:41 +0200

    code for values categories

--- app/models/yahtzee_score.rb
+++ app/models/yahtzee_score.rb
@@ -6,9 +6,17 @@ class Yahtzee_score
 				return sum_of(dices, 1)
 			when :twos
 				return sum_of(dices, 2)
+			when :threes
+				return sum_of(dices, 3)
+			when :fours
+				return sum_of(dices, 4)
+			when :fives
+				return sum_of(dices, 5)
+			when :sixes
+				return sum_of(dices, 6)
 		end
 	end
-
+ 
 	def sum_of(dices, n)
 		dices.count{|die| die == n } * n
 	end
